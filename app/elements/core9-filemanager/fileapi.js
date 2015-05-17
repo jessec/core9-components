5bca0c5de0ae7340d002d76a1412c0848502d034
jessec <robertjessecooke@gmail.com>
2015-05-17 19:12:33 +0200

    cc

--- app/elements/core9-filemanager/fileapi.js
+++ app/elements/core9-filemanager/fileapi.js
@@ -1,11 +1,24 @@
 File = {
-
-		config : function(config){
-
-		}
+	server : {},
+	db : {},
+	collection : {},
+	collectionUrl : {},
+	rest : {},
+	config : function(config) {
+		this.server = config.server;
+		this.db = config.db;
+		this.collection = config.collection;
+		this.collectionUrl = config.server + config.db + config.collection;
+		this.rest = config.rest;
+	},
+	collectionExists : function(){
+		//var collection = this.rest.GET(this.collectionUrl);
+		//console.log(collection);
+	}
 
 };
 
-
-
-export { File }
+export
+{
+	File
+}
