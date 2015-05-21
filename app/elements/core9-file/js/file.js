cc3d16f9bd6353f285de6a5fa4bd3cd06d1271e7
jessec <robertjessecooke@gmail.com>
2015-05-21 22:11:30 +0200

    cc

--- app/elements/core9-file/js/file.js
+++ app/elements/core9-file/js/file.js
@@ -3,27 +3,25 @@ File = {
 	db : {},
 	collection : {},
 	collectionUrl : {},
-	rest : {},
 	config : function(config) {
 		this.server = config.server;
 		this.db = config.db;
 		this.collection = config.collection;
 		this.collectionUrl = config.server + config.db + config.collection;
-		Core9.multiImport(
-				[ '../../../../bower_components/restful.js/dist/restful.min' ])
-				.then(function(modules) {
-					File.rest = modules[0];
-				});
 	},
 	collectionExists : function() {
-		var api = this.rest('localhost').port(8080);
-
-		api.one('core9-backend/files', 1).get().then(function(response) {
-			// console.log(response);
-		}, function(response) {
-			// The reponse code is not >= 200 and < 400
-			// console.log(response);
-			// throw new Error('Invalid response');
+		System.import('../../../../bower_components/restful.js/dist/restful.min').then(function(m) {
+			console.log('import ...');
+			console.log(m);
+			var api = m('localhost', 8080);
+			console.log(api);
+			api.one('core9-backend/files', 1).get().then(function(response) {
+				console.log(response);
+			}, function(response) {
+				// The reponse code is not >= 200 and < 400
+				console.log(response);
+				// throw new Error('Invalid response');
+			});
 		});
 	}
 
