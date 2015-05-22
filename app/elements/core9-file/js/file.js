364909f2684fa3ba9fb0cbb5f4a3f5094458bd36
jessec <robertjessecooke@gmail.com>
2015-05-22 08:34:17 +0200

    cc

--- app/elements/core9-file/js/file.js
+++ app/elements/core9-file/js/file.js
@@ -5,7 +5,6 @@ File = {
 	collection : {},
 	collectionUrl : {},
 	config : function(config) {
-		console.log(config.server);
 		File.server = config.server;
 		File.port = config.port;
 		File.db = config.db;
@@ -14,15 +13,15 @@ File = {
 	},
 	collectionExists : function() {
 		System.import('../../../../bower_components/restful.js/dist/restful.min').then(function(m) {
-			console.log('import ...');
-			console.log(m);
+			//console.log('import ...');
+			//console.log(m);
 			var api = m(File.server, File.port);
-			console.log(api);
+			//console.log(api);
 			api.one(File.collectionUrl, 1).get().then(function(response) {
-				console.log(response);
+				//console.log(response);
 			}, function(response) {
 				// The reponse code is not >= 200 and < 400
-				console.log(response);
+				//console.log(response);
 				// throw new Error('Invalid response');
 			});
 		});
